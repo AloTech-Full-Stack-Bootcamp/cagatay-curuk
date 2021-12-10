ce6e4e17ed7605de6d646c8efb2eee4ef2d5167c
cagatay135 <cagatay135@gmail.com>
2021-12-10 18:18:46 +0300

    solved week-7 python tasks

---
+++ Week-7/decorator-task.py
@@ -0,0 +1,19 @@
+"""
+Aşağıda tanımlı fonksiyonla birlikte kullanıldığında,
+  a) aldığı tüm sayı parametrelerinin değerini 1 arttıracak,
+  b) boolean dönüş değerini tersine çevirecek (True dönüyor ise False, False dönüyor ise True yapacak şekilde) bir decorator yazalım. 
+  (Not: Belki de mod_batch fonksiyonu yanlış implemente edilmiştir... Bu durumda öncelikle onun beklenen çıktıyı verecek şekilde düzenlenmesi gerekir. :))
+"""
+def decorator(func):
+    def wrapper(*args, **kwargs):
+        incresead_list = [i+1 for i in args]
+        return func(*incresead_list)
+    return wrapper
+
+
+@decorator
+def mod_batch(*numbers):
+    result = all([True if n%3==0 else False for n in numbers])
+    return result
+
+print(mod_batch(2,5,8))
---
+++ Week-7/generator-task.py
@@ -0,0 +1,19 @@
+import random
+
+"""
+Parametre olarak verilen l rakamdan (basamak sayısı) oluşan, yine parametre olarak verilen n adet rastgele sayı üreten bir generator yazalım.
+Daha önce üretilen bir sayının tekrar üretilmemesini bekliyoruz. Fonksiyonun signature'ı: random_number_generator(n, l=6)
+"""
+
+def random_number_generator(n,l=2):
+    try :
+        x = random.sample(range(10**(l-1),(10**l)-1),n)
+        for i in x:
+            yield i
+    except ValueError: 
+        print(f"{n} number cannot be produced in this range {10**(l-1)}-{(10**l)-1}")
+
+    
+for i in random_number_generator(90):
+    print(i)
+

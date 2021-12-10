59869a20ecd3b25904d3cd5d5a66005f134e3e0c
cagatay135 <cagatay135@gmail.com>
2021-12-10 23:33:08 +0300

    generator-task line 11 not problem solved

--- Week-7/decorator-task.py
+++ Week-7/decorator-task.py
@@ -4,16 +4,18 @@ Aşağıda tanımlı fonksiyonla birlikte kullanıldığında,
   b) boolean dönüş değerini tersine çevirecek (True dönüyor ise False, False dönüyor ise True yapacak şekilde) bir decorator yazalım. 
   (Not: Belki de mod_batch fonksiyonu yanlış implemente edilmiştir... Bu durumda öncelikle onun beklenen çıktıyı verecek şekilde düzenlenmesi gerekir. :))
 """
+
 def decorator(func):
     def wrapper(*args, **kwargs):
-        incresead_list = [i+1 for i in args]
-        return func(*incresead_list)
+        increased_list = [i+1 for i in args]
+        return not func(*increased_list)
     return wrapper
 
 
 @decorator
 def mod_batch(*numbers):
-    result = all([True if n%3==0 else False for n in numbers])
+    result = all([True if n % 3 == 0 else False for n in numbers])
     return result
 
-print(mod_batch(2,5,8))
+
+print(mod_batch(2, 7, 8))
--- Week-7/generator-task.py
+++ Week-7/generator-task.py
@@ -5,15 +5,15 @@ Parametre olarak verilen l rakamdan (basamak sayısı) oluşan, yine parametre o
 Daha önce üretilen bir sayının tekrar üretilmemesini bekliyoruz. Fonksiyonun signature'ı: random_number_generator(n, l=6)
 """
 
-def random_number_generator(n,l=2):
-    try :
-        x = random.sample(range(10**(l-1),(10**l)-1),n)
+
+def random_number_generator(n, l=2):
+    try:
+        x = random.sample(range(10**(l-1), (10**l)-1), n)
         for i in x:
             yield i
-    except ValueError: 
+    except ValueError:
         print(f"{n} number cannot be produced in this range {10**(l-1)}-{(10**l)-1}")
 
-    
-for i in random_number_generator(90):
-    print(i)
 
+for i in random_number_generator(50):
+    print(i)
